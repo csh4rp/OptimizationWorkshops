9f5b225cedc021881135bd447523e8a864279917
csharp <bieszczyk2@gmail.com>
2023-07-01 22:46:00 +0200

    Added second workshop exercise

---
+++ Workshop2/OptimizationWorkshops.Workshop2.GcModes/DataFrame.cs
@@ -0,0 +1,3 @@
+﻿namespace OptimizationWorkshops.Workshop2.GcModes;
+
+public record DataFrame(DateTimeOffset Timestamp, double X, double Y, double Z);
---
+++ Workshop2/OptimizationWorkshops.Workshop2.GcModes/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics.Metrics;
+using OptimizationWorkshops.Workshop2.GcModes;
+
+var meter = new Meter("OptimizationWorkshops.Workshop2.GcModes");
+var counter = meter.CreateCounter<long>("Operation count");
+
+var concurrentQueue = new ConcurrentQueue<DataFrame>();
+
+var producerTasks = Enumerable.Range(1, Environment.ProcessorCount * 16)
+    .Select(_ => Task.Run(async () =>
+    {
+        while (true)
+        {
+            var dataFrame = new DataFrame(DateTimeOffset.UtcNow, Random.Shared.NextDouble(), Random.Shared.NextDouble(), Random.Shared.NextDouble());
+            concurrentQueue.Enqueue(dataFrame);
+            counter.Add(1);
+            await Task.Delay(1);
+        }
+    }))
+    .ToArray();
+
+
+var consumerTasks = Enumerable.Range(1, Environment.ProcessorCount * 8)
+    .Select(_ => Task.Run(async () =>
+    {
+        while (true)
+        {
+            if (concurrentQueue.TryDequeue(out var dataFrame))
+            {
+                Console.WriteLine(dataFrame);
+                
+                // Fake some work
+                await Task.Delay(1);
+            }
+        }
+    }))
+    .ToArray();
+
+
+await Task.WhenAll(producerTasks.Concat(consumerTasks));

59a6d41d043e4615c467e74a7d0e680c358b8ee9
Kamil Bieszcz <kamil.bieszcz@digital-enabler.com>
2023-07-25 19:06:13 +0200

    Workshop 2 allocation fix

--- Workshop2/GcGenerationsApp/Program.cs
+++ Workshop2/GcGenerationsApp/Program.cs
@@ -15,7 +15,11 @@ do
     {
         case 'r':
         {
-            data.Add(new byte[1000]);
+            // Allocate few "small" arrays to do not store them on LOH
+            for (int i = 0; i < 10; i++)
+            {
+                data.Add(new byte[10_000]);
+            }
             break;
         }
         case '0':
@@ -63,17 +67,6 @@ do
         case 'f':
             data.Clear();
             GC.Collect(2, GCCollectionMode.Aggressive, true, true);
-            var fgc = GC.GetGCMemoryInfo(GCKind.Any);
-            Console.WriteLine();
-            Console.WriteLine("Gen 0 collection count: " + GC.CollectionCount(0));
-            Console.WriteLine("Gen 1 collection count: " + GC.CollectionCount(1));
-            Console.WriteLine("Gen 2 collection count: " + GC.CollectionCount(2));
-            Console.WriteLine("Gen 0 size: " + fgc.GenerationInfo[0].SizeBeforeBytes + " -> " +
-                              fgc.GenerationInfo[0].SizeAfterBytes);
-            Console.WriteLine("Gen 1 size: " + fgc.GenerationInfo[1].SizeBeforeBytes + " -> " +
-                              fgc.GenerationInfo[1].SizeAfterBytes);
-            Console.WriteLine("Gen 2 size: " + fgc.GenerationInfo[2].SizeBeforeBytes + " -> " +
-                              fgc.GenerationInfo[2].SizeAfterBytes);
             break;
     }
 

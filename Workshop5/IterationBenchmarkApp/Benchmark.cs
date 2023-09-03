fe2147438d8d1dd5d2d597007ef0c513bb4022da
csh4rp <bieszczyk2@gmail.com>
2023-09-03 15:00:40 +0200

    Updated Iteration Benchmark

--- Workshop5/IterationBenchmarkApp/Benchmark.cs
+++ Workshop5/IterationBenchmarkApp/Benchmark.cs
@@ -1,84 +1,70 @@
 ﻿using BenchmarkDotNet.Attributes;
 using IterationBenchmarkApp.Models;
+using Microsoft.VisualBasic;
 
 namespace IterationBenchmarkApp;
 
 [MemoryDiagnoser]
 public class Benchmark
 {
-    private readonly IEnumerable<DataFrame> _enumerable;
-    private readonly IEnumerable<DataFrame> _enumerableList;
-    private readonly List<DataFrame> _list = new();
-    private readonly HashSet<DataFrame> _set = new();
-    private readonly LinkedList<DataFrame> _linkedList = new();
-    private readonly Queue<DataFrame> _queue = new();
-    private readonly Stack<DataFrame> _stack = new();
-
-    public Benchmark()
-    {
-        var source = Enumerable.Range(1, 10_000)
-            .Select(i => new DataFrame
-            {
-                Id = Guid.NewGuid(),
-                Timestamp = DateTimeOffset.UtcNow,
-                X = Random.Shared.NextDouble(),
-                Y = Random.Shared.NextDouble(),
-                Z = Random.Shared.NextDouble()
-            });
-        
-        foreach (var dataFrame in source)
-        {
-            _list.Add(dataFrame);
-            _linkedList.AddLast(dataFrame);
-            _set.Add(dataFrame);
-            _queue.Enqueue(dataFrame);
-            _stack.Push(dataFrame);
-        }
-        
-        _enumerable = source;
-        _enumerableList = _list;
-    }
+    private static readonly int[] BatchSizes = {10, 100, 1000, 10_000, 100_000};
     
-    [Benchmark]
-    public void IterateOverEnumerable()
+    [Benchmark(Description = "Enumerable - using foreach")]
+    [ArgumentsSource(nameof(Enumerables))]
+    public void IterateOverEnumerable(IEnumerable<DataFrame> dataFrames)
     {
         DataFrame value;
         
-        foreach (var item in _enumerable)
+        foreach (var item in dataFrames)
         {
             value = item;
         }
     }
     
-    [Benchmark]
-    public void IterateOverList()
+    [Benchmark(Description = "List - using foreach")]
+    [ArgumentsSource(nameof(Lists))]
+    public void IterateOverListForeach(List<DataFrame> dataFrames)
     {
         DataFrame value;
         
-        foreach (var item in _list)
+        foreach (var item in dataFrames)
         {
             value = item;
         }
     }
     
-    [Benchmark]
-    public void IterateListAsEnumerable()
-    {
-        DataFrame value;
-        
-        foreach (var item in _enumerableList)
-        {
-            value = item;
-        }
-    }
-    
-        
-    [Benchmark]
-    public void IterateAsIReadonlyCollection()
+    [Benchmark(Description = "List - using for")]
+    [ArgumentsSource(nameof(Lists))]
+    public void IterateOverListFor(List<DataFrame> dataFrames)
     {
         DataFrame value;
 
-        var collection = _enumerableList as IReadOnlyCollection<DataFrame> ?? _enumerableList.ToList();
+        for (var i = 0; i < dataFrames.Count; i++)
+        {
+            value = dataFrames[i];
+        }
+    }
+    
+    [Benchmark(Description = "List as IEnumerable - using foreach")]
+    [ArgumentsSource(nameof(Lists))]
+    public void IterateListAsEnumerable(IEnumerable<DataFrame> dataFrames)
+    {
+        DataFrame value;
+        
+        foreach (var item in dataFrames)
+        {
+            value = item;
+        }
+    }
+    
+        
+    [Benchmark(Description = "List as IReadOnlyCollection - using foreach")]
+    [ArgumentsSource(nameof(Lists))]
+    public void IterateAsIReadonlyCollection(IEnumerable<DataFrame> dataFrames)
+    {
+        DataFrame value;
+
+        var collection = dataFrames as IReadOnlyCollection<DataFrame> ?? dataFrames.ToList();
         
         foreach (var item in collection)
         {
@@ -86,47 +72,182 @@ public class Benchmark
         }
     }
     
-    [Benchmark]
-    public void IterateOverSet()
+    [Benchmark(Description = "Set - using foreach")]
+    [ArgumentsSource(nameof(Sets))]
+    public void IterateOverSet(HashSet<DataFrame> set)
     {
         DataFrame value;
         
-        foreach (var item in _set)
+        foreach (var item in set)
         {
             value = item;
         }
     }
     
-    [Benchmark]
-    public void IterateOverQueue()
+    [Benchmark(Description = "Queue - using foreach")]
+    [ArgumentsSource(nameof(Queues))]
+    public void IterateOverQueueForeach(Queue<DataFrame> dataFrames)
     {
         DataFrame value;
         
-        foreach (var item in _queue)
+        foreach (var item in dataFrames)
         {
             value = item;
         }
     }
     
-    [Benchmark]
-    public void IterateOverStack()
+    [Benchmark(Description = "Queue - using dequeue")]
+    [ArgumentsSource(nameof(Queues))]
+    public void IterateOverQueueDequeue(Queue<DataFrame> dataFrames)
+    {
+        DataFrame? value;
+
+        while (dataFrames.TryDequeue(out value))
+        {
+            
+        }
+    }
+    
+    [Benchmark(Description = "Stack - using foreach")]
+    [ArgumentsSource(nameof(Stacks))]
+    public void IterateOverStackForeach(Stack<DataFrame> dataFrames)
     {
         DataFrame value;
         
-        foreach (var item in _stack)
+        foreach (var item in dataFrames)
         {
             value = item;
         }
     }
     
-    [Benchmark]
-    public void IterateOverLinkedList()
+    [Benchmark(Description = "Stack - using pop")]
+    [ArgumentsSource(nameof(Stacks))]
+    public void IterateOverStackPop(Stack<DataFrame> dataFrames)
+    {
+        DataFrame? value;
+
+        while (dataFrames.TryPop(out value))
+        {
+            
+        }
+    }
+    
+    [Benchmark(Description = "LinkedList - using foreach")]
+    [ArgumentsSource(nameof(LinkedLists))]
+    public void IterateOverLinkedListForeach(LinkedList<DataFrame> dataFrames)
     {
         DataFrame value;
-        
-        foreach (var item in _linkedList)
+        foreach (var item in dataFrames)
         {
             value = item;
         }
     }
+    
+    [Benchmark(Description = "LinkedList - using next")]
+    [ArgumentsSource(nameof(LinkedLists))]
+    public void IterateOverLinkedListNext(LinkedList<DataFrame> dataFrames)
+    {
+        DataFrame value;
+
+        var node = dataFrames.First;
+        while (node != null)
+        {
+            value = node.Value;
+            node = node.Next;
+        }
+    }
+    
+    public IEnumerable<object> Lists()
+    {
+        var list = Source().ToList();
+
+        foreach (var size in BatchSizes)
+        {
+            yield return list.Take(size).ToList();
+        }
+    }
+    
+    public IEnumerable<object> LinkedLists()
+    {
+        var list = Source().ToList();
+
+        foreach (var size in BatchSizes)
+        {
+            var linkedList = new LinkedList<DataFrame>();
+            for (var i = 0; i < size; i++)
+            {
+                linkedList.AddLast(list[i]);
+            }
+
+            yield return linkedList;
+        }
+    }
+    
+    public IEnumerable<object> Sets()
+    {
+        var list = Source().ToList();
+
+        foreach (var size in BatchSizes)
+        {
+            var set = new HashSet<DataFrame>();
+            for (var i = 0; i < size; i++)
+            {
+                set.Add(list[i]);
+            }
+            
+            yield return set;
+        }
+    }
+    
+    public IEnumerable<object> Queues()
+    {
+        var list = Source().ToList();
+
+        foreach (var size in BatchSizes)
+        {
+            var queue = new Queue<DataFrame>();
+            for (var i = 0; i < size; i++)
+            {
+                queue.Enqueue(list[i]);
+            }
+
+            yield return queue;
+        }
+    }
+    
+    public IEnumerable<object> Stacks()
+    {
+        var list = Source().ToList();
+
+        foreach (var size in BatchSizes)
+        {
+            var stack = new Stack<DataFrame>();
+            for (var i = 0; i < size; i++)
+            {
+                stack.Push(list[i]);
+            }
+
+            yield return stack;
+        }
+    }
+    
+    public IEnumerable<object> Enumerables()
+    {
+        var source = Source();
+
+        foreach (var size in BatchSizes)
+        {
+            yield return source.Take(size);
+        }
+    }
+
+    private static IEnumerable<DataFrame> Source() => Enumerable.Range(1, 100_000)
+        .Select(i => new DataFrame
+        {
+            Id = Guid.NewGuid(),
+            Timestamp = DateTimeOffset.UtcNow,
+            X = Random.Shared.NextDouble(),
+            Y = Random.Shared.NextDouble(),
+            Z = Random.Shared.NextDouble()
+        });
 }

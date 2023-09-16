081c7af64fd807d5fba2c533a88fb7333eb617e6
csharp <bieszczyk2@gmail.com>
2023-09-16 22:31:16 +0200

    Added ThreadPool Size Change

--- Workshop6/WebBackendApp/Program.cs
+++ Workshop6/WebBackendApp/Program.cs
@@ -1,15 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+ThreadPool.SetMinThreads(500, 500);
+
 app.MapGet("/get-async", async () =>
 {
-    await Task.Delay(1000);
+    await Task.Delay(50);
     return Results.NoContent();
 });
 
 app.MapGet("/get-sync", () =>
 {
-    Thread.Sleep(1000);
+    Thread.Sleep(50);
     return Results.NoContent();
 });
 
